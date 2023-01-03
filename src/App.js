5e34c4acf166645d8c3774399a2450a76542187f
s-ashwin <ashwin.sivanagarajan@gmail.com>
2023-01-03 20:14:13 +0530

    feat: :arrow_up: Updated antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "antd/dist/antd.css";
 import "./App.css";
 import SignIn from "./components/SignIn";
 import { useUserStore } from "./store/userStore";
