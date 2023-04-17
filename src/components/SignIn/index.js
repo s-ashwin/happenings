584bd56c5e55d3554d02bcba6e176d8a9ec5c90d
Aditya Kumar Sharma <adityakr1001@gmail.com>
2023-04-17 16:07:57 +0530

    Signin redirect issue fixed

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { supabaseClient } from "../../supabase/client";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
 import { useUserStore } from "../../store/userStore";
-import { Image } from "antd";
+import { Image, Spin } from "antd";
 import HomeArt from "../../assets/journey.svg";
 
 const Container = (props) => {
   const navigate = useNavigate();
   const user = useUserStore((state) => state.user);
+  const [isLoading, setIsLoading] = useState(false);
 
   if (user) {
+    if (isLoading) {
+      return (
+        <div className="signInPage">
+          <Spin size="large" />
+        </div>
+      );
+    }
     navigate("/mytimeline");
-    return;
+    return null;
   }
 
-  return props.children;
+  return (
+    <div className="signInPage">
+      {isLoading && (
+        <div className="overlay">
+          <Spin size="large" />
+        </div>
+      )}
+      {props.children}
+    </div>
+  );
 };
 
 export default function SignIn() {
@@ -34,6 +51,7 @@ export default function SignIn() {
             }}
             onlyThirdPartyProviders
             providers={["google"]}
+            onAuthenticated={() => setIsLoading(true)}
           />
         </Container>
       </div>
